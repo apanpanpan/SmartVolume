0283619573fd750f4e219fb44b44199a35a5990a
Angela Pan <angelapan@wireless-10-105-218-153.umd.edu>
2019-12-12 00:12:51 -0500

    added bars for time to sample audio and time to adjust volume

--- app/src/main/java/com/example/soundsmart/SettingsActivity.java
+++ app/src/main/java/com/example/soundsmart/SettingsActivity.java
@@ -3,6 +3,8 @@ package com.example.soundsmart;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +14,66 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        initControlsForAdjust();
+        initControlsForSample();
+    }
+
+    private void initControlsForAdjust(){
+        SeekBar timeAdjust = findViewById(R.id.adjustBar);
+        TextView minText = findViewById(R.id.timeAdjust);
+        minText.setText("Amount of time to adjust volume: " + timeAdjust.getProgress() + "s");
+
+
+        timeAdjust.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                TextView minText = findViewById(R.id.timeAdjust);
+                minText.setText("Amount of time to adjust volume: "+progress+"s");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+    private void initControlsForSample(){
+        try{
+            SeekBar timeSample = findViewById(R.id.sampleBar);
+
+            TextView maxText = findViewById(R.id.timeSample);
+            maxText.setText("Amount of time to sample audio: " + timeSample.getProgress() + "s");
+
+            timeSample.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    TextView maxText = findViewById(R.id.timeSample);
+                    maxText.setText("Amount of time to sample audio: "+progress+"s");
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                }
+            });
+
+        }catch(Exception e ){
+            e.printStackTrace();
+        }
+
     }
 
     public void goBack(View view) {
